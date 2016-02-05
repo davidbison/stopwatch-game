6b0de8e8f7a6a08167bee9fbc903e2b71c04695a
davidbison <davidbisonhubert@gmail.com>
2016-02-04 18:28:18 -0800

    Add timer with 0.1 interval

--- stopwatch-game.py
+++ stopwatch-game.py
@@ -1,11 +1,12 @@
-# template for "Stopwatch: The Game"
-
-
 import simplegui
 
 
 
 # define global variables
+width = 300
+height = 200
+interval = 100
+
 
 
 # define helper function format that converts time
@@ -13,20 +14,33 @@ import simplegui
 def format(t):
     pass
 
+
+
 # define event handlers for buttons; "Start", "Stop", "Reset"
 
 
+
+
 # define event handler for timer with 0.1 sec interval
+def timer_handler():
+    print interval
 
 
 # define draw handler
 
 
+
+
 # create frame
-frame = simplegui.create_frame("Stopwatch Game", 300, 200)
+frame = simplegui.create_frame("Stopwatch Game", width, height)
+
+
 
 # register event handlers
+timer = simplegui.create_timer(interval, timer_handler)
+
 
 
 # start frame
 frame.start()
+timer.start()
